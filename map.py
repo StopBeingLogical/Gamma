84b83cb437ebd01ed588870ae8cd9ec11861f1ce
Robert Raymond Alpizar <senrew@gmail.com>
2023-04-17 17:14:57 -0400

    First map

---
+++ map.py
@@ -0,0 +1,11 @@
+# Project Gamma
+# Prototype text-game engine in python
+
+# Initial Creation Date : 2023-04-15
+# by Robert Raymond Alpizar, Stop Being Logical Software
+
+# Map design working file
+
+# Items for each map room
+
+# Room ID: Unique 
